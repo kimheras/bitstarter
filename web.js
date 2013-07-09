9ca393372a3683113632c8f66c0601073207f818
Kim Heras <kimheras@gmail.com>
2013-07-09 12:37:55 +0000

    updated web.js

--- web.js
+++ web.js
@@ -9,4 +9,9 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+
+var buffer = new Buffer("Hello World from index.html", 25);
+ fs.readFileSync("index.html", "utf-8");
+response.send(resultOfreadFileSync);
+
 });
